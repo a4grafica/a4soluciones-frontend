3823d32990ad3260b848dd5d6cdfcf6904060dcc
a4grafica <barbieripabloa@gmail.com>
2025-11-21 18:43:58 -0300

    Add files via upload

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const upload = require("./middlewares/multerConfig");
+const upload = require(".multerConfig");
 const uploadCtrl = require("./controllers/uploadController");
 const jobsCtrl = require("./controllers/jobsController");
 const mpWebhookCtrl = require("./controllers/mpWebhookController");
